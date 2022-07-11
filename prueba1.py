2dc9d7b90ca91c1bc14ed80855807b156bf34f10
pekesitou <88730419+pekesitou@users.noreply.github.com>
2022-07-11 16:11:55 +0200

    Create prueba1.py

---
+++ prueba1.py
@@ -0,0 +1,71 @@
+print('-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+#Print 1
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+print("a = 1 b = 1")
+a = 1
+b = 1
+print(a == b)
+
+print('')
+print('')
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+#Print 2
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+print("c = 'Hola!' d = 'Hola!'")
+c = "Hola!"
+d = "Hola!"
+print (c == d)
+
+print('')
+print('')
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+#Print 3
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+print("e = 'Móvil' f = 'Movil'")
+e = "Móvil"
+f = "Movil"
+print (e == f)
+print('')
+print('')
+
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+#IDs
+#-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+print("id1 = id(a) id2 = id(b)")
+print('')
+id1 = id(a)
+id2 = id(b)
+print('')
+print(id(a))
+print(id(b))
+print('')
+print(id1 == id2)
+print('')
+print('')
+
+print("id3 = id(c) id4 = id(d)")
+print('')
+id3 = id(c)
+id4 = id(d)
+print('')
+print(id(c))
+print(id(d))
+print('')
+print(id(c) == id(d))
+print('')
+print('')
+
+print("id5 = id(e) id6 = id(f)")
+print('')
+id5 = id(e)
+id6 = id(f)
+print('')
+print(id(e))
+print(id(f))
+print('')
+print(id5 == id6)
+print('-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
